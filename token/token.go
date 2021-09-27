b02767226ae3b7dd777d73cd6ab9c5a8af9bf327
Jaehoon Kim <ohahaha822@gmail.com>
2021-09-27 21:34:06 +0900

    Implemented token - Defined token's types. - Defined constant values for token types.

---
+++ token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifier and Literal
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operator
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiter
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keyword
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
