a7ba558c8eb12b15f166016741a67433849b82ca
Jaehoon Kim <ohahaha822@gmail.com>
2021-09-30 22:39:05 +0900

    Update TestNextToken - For new keyword of lexer.

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -11,7 +11,10 @@ let ten = 10;
 let add = fn(x, y) {
 	x + y;
 };
+
 let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
 `
 	tests := []struct {
 		expectedType    token.TokenType
@@ -68,6 +71,7 @@ let result = add(five, ten);
 		}
 	}
 }
+
 func TestNextToken1(t *testing.T) {
 	input := `=+(){},;`
 
